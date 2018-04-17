74a3986fcbc303731a9b3b0523b17605465dd15d
michele.pilia <michele.pilia@outlook.it>
2018-04-17 15:31:02 +0200

    Aggiunta Classi java Commenti, News, User + Factory

---
+++ FPW_Project/src/java/it/unica/michelepilia/fpw/Comment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unica.michelepilia.fpw;
+import java.util.*;
+
+/**
+ *
+ * @author michelepilia
+ */
+public class Comment {
+    
+    private User author;
+    private Date date;
+    private String text;
+    
+}
---
+++ FPW_Project/src/java/it/unica/michelepilia/fpw/News.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unica.michelepilia.fpw;
+
+/**
+ *
+ * @author michelepilia
+ */
+public class News {
+
+    private int id;
+    private String title;
+    private String content;
+    private String img;
+    private String category;
+    private User author;
+
+    public News()
+    {
+        this.id = 0;
+        this.title = "Titoloplaceholder";
+        this.content= "Contenutobello";
+        this.img= "";
+        this.category = "";
+        this.author = new User();
+    }
+        
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitolo(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the img
+     */
+    public String getImg() {
+        return img;
+    }
+
+    /**
+     * @param img the img to set
+     */
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the author
+     */
+    public User getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+        
+    
+}
