e92dbc5ba674e3b598d60a115fc034c3667eb88e
michele.pilia <michele.pilia@outlook.it>
2018-04-18 17:58:16 +0200

    Implementazione classi Comment e CommentFactory

--- FPW_Project/src/java/it/unica/michelepilia/fpw/Comment.java
+++ FPW_Project/src/java/it/unica/michelepilia/fpw/Comment.java
@@ -11,9 +11,47 @@ import java.util.*;
  * @author michelepilia
  */
 public class Comment {
-    
+    private int id;
     private User author;
     private Date date;
     private String text;
     
+    public Comment () {
+        author = new User();
+        date = new Date();
+        text = "Lorem impsum dolor sit amet";
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+        
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+        
 }
--- FPW_Project/src/java/it/unica/michelepilia/fpw/CommentFactory.java
+++ FPW_Project/src/java/it/unica/michelepilia/fpw/CommentFactory.java
@@ -5,10 +5,83 @@
  */
 package it.unica.michelepilia.fpw;
 
+import java.util.*;
+
 /**
  *
  * @author michelepilia
  */
 public class CommentFactory {
+    private static CommentFactory instance;
+    private ArrayList<Comment> listComment = new ArrayList<>();
+   
+    private CommentFactory () {
+        
+        UserFactory userFactory = UserFactory.getInstance();
+
+        Comment comment1 = new Comment();
+        comment1.setAuthor(userFactory.getUserById(0));
+        comment1.setDate(new Date(2018, 3, 24, 23, 22));
+        comment1.setText ("trp bll!! XD");
+        
+        Comment comment2 = new Comment();
+        comment2.setAuthor(userFactory.getUserById(2));
+        comment2.setDate(new Date(2017, 12, 25, 12, 12));
+        comment2.setText ("buon natalo");
+        
+        Comment comment3 = new Comment();
+        comment3.setAuthor(userFactory.getUserById(1));
+        comment3.setDate(new Date(2018, 1, 12, 20, 40));
+        comment3.setText ("amen");
+        
+        
+        
+        
+        listComment.add(comment1);
+        listComment.add(comment2);
+        listComment.add(comment3);
+    }
     
+    public static CommentFactory getInstance(){
+        
+        if (instance == null)
+            instance = new CommentFactory();
+        
+        return instance;
+    }
+    
+    public ArrayList<Comment> getComments() {
+        return listComment;
+    }
+    
+    public Comment getCommentById(int id){
+        
+        for (Comment comment : listComment)
+            if (comment.getId() == id)
+                return comment;
+        
+        return null;
+    }
+    
+    public ArrayList<Comment> getCommentsByAuthor(User author) {
+        
+        ArrayList<Comment> listToReturn = new ArrayList<>();
+        
+        for (Comment comment : listComment)
+            if (author.equals(comment.getAuthor()))
+                listToReturn.add(comment);
+            
+        return listToReturn;
+    }
+    
+    public ArrayList<Comment> getCommentByDate (Date date) {
+        ArrayList<Comment> listToReturn = new ArrayList<>();
+        
+        for (Comment comment : listComment) {
+            if (date.equals(comment.getDate()))
+                listToReturn.add(comment);
+        }
+        
+        return listToReturn;
+    }
 }
